6e3639526412fed97d6efe8e55ca0326255b3798
ustegius <riku.rantala@gmail.com>
2019-03-18 10:34:43 +0200

    Fixed the timeline problem

---
+++ Kalevala_jrpg/Assets/Scripts/Cutscene/TimelineController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector director;
+    
+    void OnEnable()
+    {
+        //director.paused += OnPlayableDirectorPaused;
+        OnPlayableDirectorPaused(director);
+    }
+
+    void OnPlayableDirectorPaused(PlayableDirector aDirector)
+    {
+        if (director == aDirector)
+            aDirector.Pause();
+
+    }
+
+    void OnDisable()
+    {
+        director.paused -= OnPlayableDirectorPaused;
+    }
+}
